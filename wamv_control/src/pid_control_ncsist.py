c3bf6eda532a9154145cb3b253856d6351e3499a
hsuchaochun <leohsu210200@gmail.com>
2021-07-08 17:44:58 +0800

    update control to wamv_control

--- wamv_control/src/pid_control_ncsist.py
+++ wamv_control/src/pid_control_ncsist.py
@@ -8,7 +8,7 @@ from sensor_msgs.msg import Joy
 from geometry_msgs.msg import PoseArray, Pose, PoseStamped, Point, Twist
 from visualization_msgs.msg import Marker, MarkerArray
 from dynamic_reconfigure.server import Server
-from control.cfg import pos_PIDConfig, ang_PIDConfig
+from wamv_control.cfg import pos_PIDConfig, ang_PIDConfig
 from std_srvs.srv import SetBool, SetBoolResponse
 from nav_msgs.msg import Odometry
 from PID import PID_control
